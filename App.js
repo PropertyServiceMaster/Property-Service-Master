c2aea1240f596631b62ff25afb8945880e789a06
PropertyServiceMaster <102439160+PropertyServiceMaster@users.noreply.github.com>
2022-08-15 23:09:56 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,86 +1,77 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-
-
-
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import HomeScreen from './screens/HomeScreen';
-import Appointments from './screens/Appointments';
-import Settings from './screens/Settings';
-import Chat from './screens/Chat';  // Import the Chat component
+import Appointment from './screens/Appointment';
+import AppSettings from './screens/AppSettings';
+import Chat from './screens/Chat';
 
 
-const Tab = createMaterialBottomTabNavigator();
+
+
+const Tab = createBottomTabNavigator();
 
 function MyTabs() {
-    return (
-      <Tab.Navigator
-        initialRouteName="Home"
-        activeColor="#fff"
-      >
-        <Tab.Screen
-          name="Feed"
-          component={HomeScreen}
-          options={{
-            tabBarLabel: 'Home',
-            tabBarColor: '#fff',
-            tabBarIcon: ({ color }) => (
-              <Icon name="home" color={color} size={35} />
-            ),
-          }}
-        />
-        
-        <Tab.Screen
-          name="Settings"
-          component={Settings}
-          options={{
-            tabBarLabel: 'Settings',
-            tabBarColor: '#1f65ff',
-            tabBarIcon: ({ color }) => (
-              <Icon name="account" color={color} size={26} />
-            ),
-          }}
-        />
-         <Tab.Screen
-          name="Chat"
-          component={Chat}
-          options={{
-            tabBarLabel: 'Chat',
-            tabBarColor: '#694fad',
-            tabBarIcon: ({ color }) => (
-              <Icon name="account" color={color} size={26} />
-            ),
-          }}
-        />
-         <Tab.Screen
-          name="Appointments"
-          component={Appointments   }
-          options={{
-            tabBarLabel: 'Appointments',
-            tabBarColor: '#d02860',
-            tabBarIcon: ({ color }) => (
-              <Icon name="home" color={color} size={26} />
-            ),
-          }}
-        />
-      </Tab.Navigator>
-    );
-  }
+  return (
+    <Tab.Navigator
+      initialRouteName="Feed"
+      activeColor="#e91e63"
+      labelStyle={{ fontSize: 12 }}
+      style={{ backgroundColor: 'tomato' }}
+    >
+      <Tab.Screen
+        name="Home Screen"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarColor: '#62a725',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }}
+      />
+       <Tab.Screen
+        name="Appointments "
+        component={Appointment}
+        options={{
+          tabBarLabel: 'Appointments',
+          tabBarColor: '#1f65ff',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="bell" color={color} size={26} />
+          ),
+        }}
+      />
+     <Tab.Screen
+        name="Settings"
+        component={AppSettings}
+        options={{
+          tabBarLabel: 'Settings',
+          tabBarColor: '#ee0bf5',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="account" color={color} size={26} />
+          ),
+        }}
+      />
+     <Tab.Screen
+        name="Chat"
+        component={Chat}
+        options={{
+          tabBarLabel: 'Chat',
+          tabBarColor: '#f59e0b',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="chat" color={color} size={26} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
 
-
-  const app = () => {
-    return (
-      <NavigationContainer>
-        <Tab.Navigator initialRouteName="Home">
-          <Tab.Screen name="Home" component={HomeScreen} />
-          <Tab.Screen name="Appointments" component={Appointments} />
-          <Tab.Screen name="Settings" component={Settings} />
-          <Tab.Screen name="Chat" component={Chat} />
-        </Tab.Navigator>
-      </NavigationContainer>
-    );
-  }
-
-  export default app;
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
