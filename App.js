b0eb8178e8c95630561f6065f427750cca82cdc2
PropertyServiceMaster <102439160+PropertyServiceMaster@users.noreply.github.com>
2022-09-03 10:13:08 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,77 +1,9 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import HomeScreen from './screens/HomeScreen';
-import Appointment from './screens/Appointment';
-import AppSettings from './screens/AppSettings';
-import Chat from './screens/Chat';
+import React from 'react';
 
+import Providers from './navigation';
 
-
-
-const Tab = createBottomTabNavigator();
-
-function MyTabs() {
-  return (
-    <Tab.Navigator
-      initialRouteName="Feed"
-      activeColor="#e91e63"
-      labelStyle={{ fontSize: 12 }}
-      style={{ backgroundColor: 'tomato' }}
-    >
-      <Tab.Screen
-        name="Home Screen"
-        component={HomeScreen}
-        options={{
-          tabBarLabel: 'Home',
-          tabBarColor: '#62a725',
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="home" color={color} size={26} />
-          ),
-        }}
-      />
-       <Tab.Screen
-        name="Appointments "
-        component={Appointment}
-        options={{
-          tabBarLabel: 'Appointments',
-          tabBarColor: '#1f65ff',
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="bell" color={color} size={26} />
-          ),
-        }}
-      />
-     <Tab.Screen
-        name="Settings"
-        component={AppSettings}
-        options={{
-          tabBarLabel: 'Settings',
-          tabBarColor: '#ee0bf5',
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="account" color={color} size={26} />
-          ),
-        }}
-      />
-     <Tab.Screen
-        name="Chat"
-        component={Chat}
-        options={{
-          tabBarLabel: 'Chat',
-          tabBarColor: '#f59e0b',
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="chat" color={color} size={26} />
-          ),
-        }}
-      />
-    </Tab.Navigator>
-  );
+const App = () => {
+  return <Providers />;
 }
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <MyTabs />
-    </NavigationContainer>
-  );
-}
+export default App;
